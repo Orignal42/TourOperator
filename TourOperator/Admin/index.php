c7d20a4d4634c556604c193db2d9c2f1a36e0626
Orignal42 <77976626+Orignal42@users.noreply.github.com>
2021-05-12 07:40:16 +1400

    Add files via upload

---
+++ TourOperator/Admin/index.php
@@ -0,0 +1,50 @@
+<?php
+
+    include __DIR__.'/../Process/Autoload.php';
+
+    require_once __DIR__."/../Process/Connexion.php";
+
+
+
+    include __DIR__."/../View/Header.php";
+
+?>
+
+
+<div class="container forms-admin">
+    <div class="row">
+        <div class="col-lg-6 col-md-12 col-sm-12 col-md-offset-4">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="text-center">
+                        <img src="/IMG/admin.png" border="0">
+                        <h2 class="text-center">Connect to access </h2>
+
+                        <form id="register-form" role="form" autocomplete="off" class="form" method="post" action="/Process/sessionStart.php">
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <span class="input-group-addon"><i class="glyphicon glyphicon-envelope color-blue"></i></span>
+                                    <input id="forgetAnswer" name="pseudoAdmin" placeholder="Name" class="form-control"  type="text" required>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <span class="input-group-addon"><i class="glyphicon glyphicon-envelope color-blue"></i></span>
+                                    <input id="forgetAnswer" class="form-control" type="password" name="password" placeholder="password" required>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <input name="btnForget" class="btn btn-lg btn-primary btn-block btnForget"type="submit">
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php include __DIR__."/../View/Footer.php";
+    
---
+++ TourOperator/Classes/DestinationManager.php
@@ -0,0 +1,108 @@
+<?php
+
+class DestinationManager {
+
+  private $db;
+
+  public function __construct($db)
+  {
+    $this->db = $db;
+  }
+
+  public function add(Destination $destination, TourOperator $tour_operator)
+  {
+ 
+    $q = $this->db->prepare('INSERT INTO destinations(location, price, id_tour_operator, img, description) VALUES(:location, :price, :id_tour_operator, :img, :description)');
+    
+    $q->bindValue(':location', $destination->getLocation());
+    $q->bindValue(':price', $destination->getPrice());
+    $q->bindValue(':id_tour_operator', $tour_operator->getId());
+    $q->bindValue(':description', $destination->getDescription());
+    $q->bindValue(':img', $destination->getImg());
+    
+    $q->execute();
+    
+    $destination->hydrate([
+      'id' => $this->db->lastInsertId()
+    ]);
+  }
+
+  /* RECUPERER DESTI POUR LES AFFICHER */
+
+  public function getList()
+  {
+    $desti = [];
+    
+    $q = $this->db->prepare('SELECT * FROM destinations');
+    $q->execute();
+    
+    while ($donnees = $q->fetch(PDO::FETCH_ASSOC))
+    {
+      echo '<br>';
+      array_push($desti, new Destination ($donnees));
+    }
+    
+    return $desti;
+  }
+
+    /* JOIN DESTINATIONS W/ TO */
+
+  public function getDestibyTo(Destination $destination)
+  {
+
+    $q = $this->db->prepare('SELECT * FROM tour_operators WHERE id=?');
+      
+    
+    $q->execute([$destination->getIdTourOperator()]);
+    $To = $q->fetch(PDO::FETCH_ASSOC);
+    $test = new TourOperator($To);
+
+    return $test;
+  }
+
+  public function getDestinationByLocation($location)
+  {
+
+    $destinationCollection = [];
+
+    $q = $this->db->prepare('SELECT * FROM destinations WHERE location=?');
+      
+    
+    $q->execute([$location]);
+    $destinations = $q->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($destinations as $destinationArray) {
+      array_push($destinationCollection, new Destination($destinationArray));
+    }
+
+    return $destinationCollection;
+  }
+
+  /* METHODE POUR PAS AVOIR DE DOUBLON */
+
+  public function getListGroupByName()
+  {
+
+    $q = $this->db->prepare('SELECT location, img, description FROM destinations GROUP BY location, img, description');
+
+    $destinations = [];
+
+    $q->execute();
+
+    while ($donnees = $q->fetch(PDO::FETCH_ASSOC))
+    {
+      array_push($destinations, new Destination ($donnees));
+    }
+      return $destinations;
+  }
+
+
+  /* METHODE POUR SUP UNE DESTI */
+
+  public function DeleteDestination(Destination $destination){
+    $q= $this->db->prepare('DELETE  FROM destinations WHERE id= :id');
+    $q->bindValue(':id', $destination->getId());
+    $q->execute();
+  }
+  
+
+}
